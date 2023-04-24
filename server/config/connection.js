14d3e4036d69bbd5dbdaeac652d033e4dac969ff
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2023-04-24 20:01:33 +0530

    Add mongodb conection options

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,15 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://0.0.0.0:27017/color-picker");
+    const options = {
+      connectTimeoutMS: 30000, // 30 seconds
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    };
+    const conn = await mongoose.connect(
+      "mongodb://0.0.0.0:27017/color-picker",
+      options
+    );
     console.log(`Mongodb Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error.message);
