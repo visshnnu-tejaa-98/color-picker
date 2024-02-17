c7fc31cf2cefeccb170d020ab422217cedcd3787
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2024-02-17 15:56:19 +0530

    Change Origin

--- src/config/config.development.js
+++ src/config/config.development.js
@@ -1,7 +1,7 @@
 const protocol = "http:";
 const host = "//localhost:8000";
-const origin = "http://localhost:8000";
-// const origin = "https://color-picker-server-production.up.railway.app";
+// const origin = "http://localhost:8000";
+const origin = "https://color-picker-server-production.up.railway.app";
 
 const DEV_API = {
   getAllTwoToneGradients: {
