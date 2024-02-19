bf89894296ea4a33fd1109168c5ff123e604047e
visshnnu-tejaa-98 <74036897+visshnnu-tejaa-98@users.noreply.github.com>
2024-02-19 19:51:40 +0530

    Restrict for mobile devices (#36)

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import { ColorsContextProvider } from "./contexts/colorsContext";
 import LandingPage from "./components/LandingPage";
 import { ApiColorsContextProvider } from "./contexts/apiColorsContext";
 import "flowbite";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [showMobileWarning, setShowMobileWarning] = useState("");
+
+  useEffect(() => {
+    if (window.innerWidth <= 800)
+      setShowMobileWarning("Please open this Application in Desktop/Laptop");
+  }, []);
   return (
     <>
-      <ColorsContextProvider>
-        <ApiColorsContextProvider>
-          <LandingPage />
-        </ApiColorsContextProvider>
-      </ColorsContextProvider>
+      {showMobileWarning ? (
+        <div className="w-[100vw] h-[100vh] bg-white">
+          <div className="flex justify-center h-[100vh] items-center">
+            <h1 className="text-2xl text-center">{showMobileWarning}</h1>
+          </div>
+        </div>
+      ) : (
+        <ColorsContextProvider>
+          <ApiColorsContextProvider>
+            <LandingPage />
+          </ApiColorsContextProvider>
+        </ColorsContextProvider>
+      )}
     </>
   );
 }
