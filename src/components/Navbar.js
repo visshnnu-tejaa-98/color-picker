61bc51d12e13e1df7d50bfc05220a8774f8f37d6
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2024-02-25 16:00:45 +0530

    Add Profile Image in header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-import { Footer } from "flowbite-react";
 import React, { useContext, useEffect, useState } from "react";
 import {
   Link,
@@ -27,6 +26,7 @@ const Navbar = () => {
     data: null,
     errorMessage: null,
   });
+  const [userData, setUserData] = useState(null);
   const ApiColorsCtx = useContext(ApiColorsContext);
   const navigate = useNavigate();
 
@@ -58,8 +58,11 @@ const Navbar = () => {
       ApiColorsCtx.getAuthToken() !== "undefined"
     ) {
       setIsLoggedIn(true);
+      setUserData(ApiColorsCtx.getUser());
     } else {
       setIsLoggedIn(false);
+      localStorage.removeItem("user");
+      localStorage.removeItem("colorPicker");
     }
   }, [ApiColorsCtx.getAuthToken()]);
 
@@ -198,7 +201,7 @@ const Navbar = () => {
                     Sign In
                   </NavLink>
                 </li>
-                <li className={`${!isLoggedIn && "hidden"}`}>
+                {/* <li className={`${!isLoggedIn && "hidden"}`}>
                   <NavLink
                     to="/"
                     className={({ isActive }) =>
@@ -210,7 +213,7 @@ const Navbar = () => {
                   >
                     Sign Out
                   </NavLink>
-                </li>
+                </li> */}
                 <li>
                   <NavLink
                     to="/solid"
@@ -322,6 +325,86 @@ const Navbar = () => {
                     )}
                   </div>
                 </li>
+                {userData && (
+                  <div className={`${!isLoggedIn && "hidden"}`}>
+                    <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                      <button
+                        type="button"
+                        class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-2 focus:ring-gray-300"
+                        id="user-menu-button"
+                        aria-expanded="false"
+                        data-dropdown-toggle="user-dropdown"
+                        data-dropdown-placement="bottom"
+                      >
+                        <span class="sr-only">Open user menu</span>
+                        <div className="h-8 w-8 bg-[#FCD34D] text-[#222222] font-bold text-xl rounded-full flex justify-center items-center">
+                          <div>{userData.name.slice(0, 1).toUpperCase()}</div>
+                        </div>
+                        {/* <img
+                      class="w-8 h-8 rounded-full"
+                      src="/docs/images/people/profile-picture-3.jpg"
+                      alt="user photo"
+                    /> */}
+                      </button>
+                      <div
+                        class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow"
+                        id="user-dropdown"
+                      >
+                        <div class="px-4 py-3">
+                          <span class="block text-sm text-gray-900">
+                            {userData.name}
+                          </span>
+                          <span class="block text-sm  text-gray-500 truncate">
+                            {userData.email}
+                          </span>
+                        </div>
+                        <ul class="py-2" aria-labelledby="user-menu-button">
+                          <li>
+                            <Link
+                              to="/dashboard"
+                              class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                            >
+                              Dashboard
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              to="#"
+                              class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                              onClick={handleSignOut}
+                            >
+                              Sign Out
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
+                      <button
+                        data-collapse-toggle="navbar-user"
+                        type="button"
+                        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                        aria-controls="navbar-user"
+                        aria-expanded="false"
+                      >
+                        <span class="sr-only">Open main menu</span>
+                        <svg
+                          class="w-5 h-5"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 17 14"
+                        >
+                          <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M1 1h15M1 7h15M1 13h15"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                )}
               </ul>
             </div>
           </div>
